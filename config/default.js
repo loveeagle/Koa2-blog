4f72262696431a0706f2fcab6def4aab846820ea
eagle <13710294423@163.com>
2018-05-12 17:04:53 +0800

    存在BUG，没法删除文章和发表评论

---
+++ config/default.js
@@ -0,0 +1,14 @@
+const config = {
+  // 启动端口
+  port: 1337,
+  // 数据库配置
+  database: {
+    DATABASE: 'nodesql',
+    USERNAME: 'root',
+    PASSWORD: 'sy559649',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
